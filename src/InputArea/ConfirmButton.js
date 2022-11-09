8887824fe43eafa40402bb4bb9dd5c4445fb048b
Petr Rikhter <petr_rikhter@mail.ru>
2022-11-09 22:20:08 +0600

    added InputArea Components

---
+++ src/InputArea/ConfirmButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ConfirmButton(props) {
+  return <div></div>;
+}
+
+export default ConfirmButton;
---
+++ src/InputArea/InputArea.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function InputArea(props) {
+  return <div></div>;
+}
+
+export default InputArea;
